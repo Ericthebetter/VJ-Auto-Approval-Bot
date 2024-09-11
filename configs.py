8fa98d865d87d381c8dcc50b00f75ab98241aa5a
Ericthebetter <cerolsantos96@gmail.com>
2024-09-10 22:37:02 -0300

    configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "f8b51bd3132b314c214b1d9a754500f2")
     BOT_TOKEN = getenv("BOT_TOKEN", "7494930693:AAGO5P7Gt0WX7WZt5zivgPqUrVguQmS5aiA")
     FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
+    CHID = int(getenv("CHID", "-1002452163040"))
     SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
     
