95f96ba98254eabe6154bed95d39a81c94406256
Ericthebetter <cerolsantos96@gmail.com>
2024-09-10 22:35:55 -0300

    configs.py

--- configs.py
+++ configs.py
@@ -6,9 +6,9 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20389440"))
-    API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
+    API_ID = int(getenv("API_ID", "22485565"))
+    API_HASH = getenv("API_HASH", "f8b51bd3132b314c214b1d9a754500f2")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7494930693:AAGO5P7Gt0WX7WZt5zivgPqUrVguQmS5aiA")
     FSUB = getenv("FSUB", "VJ_Botz")
     CHID = int(getenv("CHID", "-1001623633000"))
     SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
